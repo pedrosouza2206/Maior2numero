d67d801c1d7c6008e75bf54192124d92927cd2d0
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 21:31:09 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Digite o primeiro número..: 7
+//Digite o segundo número...: 15
+//O maior número é 15
+
+double primeiro, segundo;
+
+ Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Digite o primeiro número..: ");
+primeiro = Convert.ToDouble(Console.ReadLine());
+Console.ResetColor();
+ 
+ Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Digite o segundo número...: ");
+segundo = Convert.ToDouble(Console.ReadLine());
+ Console.ResetColor();
+ 
+if (primeiro > segundo)
+{
+    Console.WriteLine($"O maior número é {primeiro}");
+}
+else
+{
+    Console.WriteLine($"O maior número é {segundo}");
+}
